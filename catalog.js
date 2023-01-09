b96ad06e5462ad06e53350c357a8d753fb46d650
Erwan MARTIN <erwan.martin@viacesi.fr>
2023-01-09 23:52:31 +0100

    preparing for db connexion

--- catalog.js
+++ catalog.js
@@ -38,25 +38,50 @@ app.get("/catalog/:idr/menus", (req,res)=>{
 });
 
 app.get("/catalog/:idr/menus/:numm", (req,res)=>{
-    var id = req.params.id;
+    var idr = req.params.id;
     var numm = req.params.id;
     //montrer un menu spécifique de la boutique id
     //CLIENT: READ ONLY
     //RESTO: CREATE READ UPDATE DELETE
 });
 app.get("/catalog/:idr/menus", (req,res)=>{
-    var id = req.params.id;
+    var idr = req.params.id;
     //montrer les menus de la boutique id
     //CLIENT: READ ONLY
     //RESTO: CREATE READ UPDATE DELETE
 });
 
 app.get("/catalog/:idr/menus/:numa", (req,res)=>{
-    var id = req.params.id;
+    var idr = req.params.id;
     var numa = req.params.id;
     //montrer un menu spécifique de la boutique id
     //CLIENT: READ ONLY
     //RESTO: CREATE READ UPDATE DELETE
+});
+app.post("/catalog/:idr/menus/:numa", (req,res)=>{
+    var idr = req.params.id;
+    var numa = req.params.id;
+
+
+
+    var newMenu = {
+        id:req.body.id,
+        type:req.body.type,
+        datas:req.body.datas,
+        metrics:req.body.metrics
+    }
+    var sensor = sensors.find((sensor)=>{
+        return sensor.id == newSensor.id
+    })
+    if(sensor){
+        res.status(409).json({message:"l'id est deja pris"})
+    }
+    else{
+        //sensors.push(newSensor);
+        db.sensor.insertMany(newSensor)
+        res.status(200).json({message:`l'id ${newSensor.id} a bien été ajouté`})
+        console.log(sensors)
+    }
 });
 
 
